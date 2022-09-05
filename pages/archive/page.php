97e163d56ac7dc578ce7b246e5fd6c328982a6d9
Leora Wenger <leora.wenger@mtahq.org>
2022-09-05 08:34:35 -0400

    added archive twig for category.

---
+++ pages/archive/page.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace timber_hp_theme\timber_hp_theme;
+
+use Timber;
+
+/** Stop executing files when accessing them directly */
+if ( ! defined( 'ABSPATH' ) ){
+	die( 'Direct access to theme files is not allowed.' );
+}
+
+global $wp_query;
+
+if ( is_archive() ){
+	if ( is_post_type_archive() ){
+
+	} elseif ( is_tax( 'category' ) ) {
+
+	} elseif ( is_tax( 'post_tag' ) ) {
+
+	}
+}
+
+$context          = Timber::context();
+$context['posts'] = Timber::get_posts( $wp_query );
+$templates        = [ "archive.twig" ];
+
+
+Timber::render(
+	$templates,
+	$context
+);
