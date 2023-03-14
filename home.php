f42f88b2f5d3e419c45383876c09cabf66f28c94
Leora Wenger <leora.wenger@mtahq.org>
2023-03-14 07:54:55 -0400

    added teasers to home news articles twig.

--- home.php
+++ home.php
@@ -4,9 +4,4 @@ if ( ! defined( 'ABSPATH' ) ){
 	die( 'Direct access to theme files is not allowed.' );
 }
 
-// $currentPage = get_page_by_title('Home Sweet Home');
-// print_r($currentPage);
-
 get_template_part( 'pages/home/news' );
-// get_template_part( 'pages/home/page' );
-// get_template_part( 'sidebar' );
